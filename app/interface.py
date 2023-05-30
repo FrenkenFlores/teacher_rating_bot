8152ce60cfcc2eb8da04f32c52ee4bea03a79193
FrenkenFlores <saifualdin.evloev@gmail.com>
2023-05-30 19:12:27 +0300

    update #4: * add the authenticate method * document code

--- app/interface.py
+++ app/interface.py
@@ -1,22 +1,70 @@
+import logging
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
-from aiogram import Dispatcher
+from aiogram import Dispatcher, Bot
+from OptimizedDispatcher import OptimizedDispatcher
 import messages
 
 class StartKeyboard:
-    def __init__(self, dp: Dispatcher):
+    BUTTON_1 = "add_teacher"
+    BUTTON_2 = "rate_teacher"
+
+    def __init__(self, odp: Dispatcher):
         self.keyboard = InlineKeyboardMarkup()
-        self.button_rate_teacher = InlineKeyboardButton(text=messages.BUTTON_RATE_TEACHER, callback_data="rate_teacher")
-        self.button_add_teacher = InlineKeyboardButton(text=messages.BUTTON_ADD_TEACHER, callback_data="add_teacher")
+        self.button_rate_teacher = InlineKeyboardButton(text=messages.BUTTON_ADD_TEACHER, callback_data="add_teacher")
+        self.button_add_teacher = InlineKeyboardButton(text=messages.BUTTON_RATE_TEACHER, callback_data="rate_teacher")
         self.keyboard.add(self.button_add_teacher)
         self.keyboard.add(self.button_rate_teacher)
+        self.odp = odp
 
-        @dp.callback_query_handler(text="add_teacher")
+        @self.odp.callback_query_handler(text="rate_teacher")
+        async def rate_teacher(callback: CallbackQuery):
+            bot_id = callback.message["from"]["id"]
+            group_id = callback.message["chat"]["id"]
+            valid, msg = await self.check_groups(self, bot_id, group_id)
+            if valid:
+                await callback.answer(text="оценть")
+                await callback.message.answer(msg)
+            else:
+                await callback.answer(text="Ошибка")
+                await callback.message.answer(msg)
+
+        @self.odp.callback_query_handler(text="add_teacher")
         async def add_teacher(callback: CallbackQuery):
-            await callback.answer(text="добавить")
-            await callback.message.answer("Добавить преподавателя")
-
-        @dp.callback_query_handler(text="rate_teacher")
-        async def add_teacher(callback: CallbackQuery):
-            await callback.answer(text="оценть")
-            await callback.message.answer("Оценить преподавателя")
-
+            bot_id = callback.message["from"]["id"]
+            group_id = callback.message["chat"]["id"]
+            valid, msg = await self.check_groups(self, bot_id, group_id, check_admin=True)
+            if valid:
+                await callback.answer(text="добавить")
+                await callback.message.answer(msg)
+            else:
+                await callback.answer(text="Ошибка")
+                await callback.message.answer(msg)
+    @staticmethod
+    async def check_groups(instance, bot_id: str, user_id: str, check_admin=False):
+        """Check that the bot and user belong to the same group.
+        This funtcion returns a tuple that holds boolean indicator and string message."""
+        groups: dict = instance.odp.get_groups()
+        # Check if the bot is added to any group.
+        if not groups:
+            return False, messages.TYPE_TEACHER_NAME_ERROR_NO_GROUP
+        # If there is groups.
+        else:
+            bot = Bot.get_current()
+            # Iterate over the ID of the group.
+            for group in groups.keys():
+                # Check if the user and group belong to the current group.
+                try:
+                    bot_member = await bot.get_chat_member(int(group), int(bot_id))
+                    user_member = await bot.get_chat_member(int(group), int(user_id))
+                except Exception as e:
+                    logging.error(e)
+                    return False, messages.TYPE_TEACHER_NAME_ERROR_BOT_KICKED
+                if not bot_member.is_chat_member():
+                    continue
+                if not user_member.is_chat_member():
+                    continue
+                elif user_member.is_chat_member():
+                    if check_admin and not user_member.is_chat_admin():
+                        return False, messages.TYPE_TEACHER_NAME_ERROR_NO_ADMIN
+                return True, (messages.HANDLER_ADD_TEACHER if check_admin else messages.HANDLER_RATE_TEACHER)
+        return False, messages.TYPE_TEACHER_NAME_ERROR_NO_USER
--- app/main.py
+++ app/main.py
@@ -1,22 +1,48 @@
+import typing
+from aiohttp.helpers import sentinel
+import aiohttp
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.dispatcher.filters import Text
 from credentials import TOKEN
 import messages
 import interface
 import logging
+import asyncio
+import json
+import copy
+import pprint
+import time
+from aiogram.types import BotCommandScope, BotCommandScopeAllGroupChats
+from OptimizedDispatcher import OptimizedDispatcher
 
 
+# Create Bot instance, Bot class inherits from BaseBot class that accepts in the __init__() magic method the
+# Telegram token. The token will be stored in Contextvar "BotDifferentToken". It will be used when a request will be
+# sent to https://api.telegram.org/bot<token>/METHOD_NAME with the aiohttp module. In general, Bot is the main interface
+# to interact with Telegram API. Bot current instance can be retrieved from anyway with Bot.get_current() method,
+# metadata can be retrieved using Bot.get_me() method.
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot=bot)
+# OptimizedDispatcher is a customized class that inherits from Dispatcher class, the main difference is that the current
+# one tracks events when the bot gets added to a group or supergroup and save the event results in its instance
+# properties. That way we can track the groups where our bot was added, what permissions it has, and is it available at
+# the moment. Dispatcher is needed to monitor the events and updates that occur and control how the bot function.
+odp = OptimizedDispatcher(bot=bot)
 
 
 async def on_bot_startup(_):
+    """This function will be passed to executor.start_pooling() method, it will be executed once the bot starts, it can
+    be used to initialize the bot. Also, Aiogram uses Asyncio, the start_pooling() method calls asyncio.get_event_loop()
+    then loop.create_task() and after that loop.run_forever(); so, we can add our own async tasks in this function if
+    we want them to run asynchronously using asyncio.create_task(some_loop_fun())."""
     meta = await bot.get_me()
     logging.debug(meta)
     logging.info(f"Started bot with id={meta['id']}, first_name={meta['first_name']}, username={meta['username']}")
 
-@dp.message_handler(commands=["start"])
+
+@odp.message_handler(commands=["start"])
 async def start_handler(message: types.Message):
-    kb = interface.StartKeyboard(dp).keyboard
+    odp = Dispatcher.get_current()
+    kb = interface.StartKeyboard(odp).keyboard
     await message.answer(
         text=messages.START_MSG,
         parse_mode="HTML",
@@ -25,14 +51,22 @@ async def start_handler(message: types.Message):
     await message.delete()
 
 
+@odp.message_handler(Text(equals=messages.HANDLER_ADD_TEACHER))
+async def start_handler(message: types.Message):
+    await message.delete()
+
+
 def main():
     logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
     executor.start_polling(
-        dispatcher=dp,
+        dispatcher=odp,
         skip_updates=True,
         on_startup=on_bot_startup
     )
 
 
+# When the current file gets passed to the interpreter, the __name__ global variable will be set to "__main__". That
+# means that the main function will only run, when the current file will be executed.
 if __name__ == "__main__":
     main()
+
